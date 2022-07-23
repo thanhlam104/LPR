dcaf739cb4510d361c5cc527d6347b4de1eb7d5b
thanhlam104 <91646846+thanhlamdang@users.noreply.github.com>
2022-07-23 17:03:37 +0700

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,11 +20,12 @@ if __name__=='__main__':
 
         img = cv2.imread('img.jpg')
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img2 = img
         st.subheader('Character-based method')
         st.image(lpr.end2end_character_based(img))
 
         st.subheader('Segment-based method')
-        st.image(lpr.end2end_segment_based(img))
+        st.image(lpr.end2end_segment_based(img2))
 
 
 
